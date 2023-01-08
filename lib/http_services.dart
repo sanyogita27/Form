3b39ff6184c40be658858ca6097585a02395055e
sanyogita27 <sanyogitachand2@gmail.com>
2023-01-09 00:34:43 +0545

    files added

---
+++ lib/http_services.dart
@@ -0,0 +1,101 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:login_form1/model.dart';
+
+class HttpService {
+  static const urlPrefix =
+      'https://console.firebase.google.com/u/0/project/login-form-4286a/firestore';
+  final postsUrl = Uri.parse('$urlPrefix/data');
+  Future<List<UserModel>> getPosts() async {
+    Response res = await get(postsUrl);
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+      List<UserModel> users =
+          body.map((dynamic item) => UserModel.fromJson(item)).toList();
+      return users;
+    } else {
+      throw 'Unable to retrieve the information';
+    }
+  }
+
+  Future<void> deletePost() async {
+    Response res = await delete(postsUrl);
+    if (res.statusCode == 200) {
+      debugPrint('Deleted');
+    } else {
+      throw "Unable to delete";
+    }
+  }
+
+  Future<List<UserModel>> putposts() async {
+    Response res = await get(postsUrl);
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+      List<UserModel> users =
+          body.map((dynamic item) => UserModel.fromJson(item)).toList();
+      return users;
+    } else {
+      throw 'Unable to edit';
+    }
+  }
+
+  Future<List<UserModel>> post() async {
+    Response res = await get(postsUrl);
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+      List<UserModel> users =
+          body.map((dynamic item) => UserModel.fromJson(item)).toList();
+      return users;
+    } else {
+      throw 'Unable to add';
+    }
+  }
+}
+
+class DetailInfo extends StatelessWidget {
+  final HttpService httpService = HttpService();
+  final UserModel users;
+
+  DetailInfo({super.key, required this.users});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('${users.uid}'),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                httpService.deletePost;
+              },
+              icon: const Icon(Icons.delete),
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              children: <Widget>[
+                Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      ListTile(
+                        title: const Text("Email"),
+                        subtitle: Text('${users.email}'),
+                      ),
+                      ListTile(
+                        title: const Text("Phone"),
+                        subtitle: Text("${users.phone}"),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/information_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:login_form1/global.dart';
+import 'package:login_form1/http_services.dart';
+import 'package:login_form1/model.dart';
+
+class InformationPage extends StatelessWidget {
+  InformationPage({super.key});
+  final HttpService httpService = HttpService();
+  Future signOut() async {
+    try {
+      return firebaseAuth.signOut();
+    } catch (e) {
+      debugPrint(e.toString());
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Posts"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                signOut();
+              },
+              icon: const Icon(Icons.logout))
+        ],
+      ),
+      body: FutureBuilder(
+        future: httpService.getPosts(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<UserModel>> snapshot) {
+          if (snapshot.hasData) {
+            List<UserModel>? users = snapshot.data;
+            return ListView(
+              children: users!
+                  .map(
+                    (UserModel users) => ListTile(
+                        title: Text("${users.email}"),
+                        subtitle: Text("${users.uid}"),
+                        trailing: Text('${users.phone}'),
+                        onTap: () => Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    DetailInfo(users: users)))),
+                  )
+                  .toList(),
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
